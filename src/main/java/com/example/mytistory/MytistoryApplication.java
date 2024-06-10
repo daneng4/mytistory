46d2d3d0677c3f28ba309e6b280d7c8cbaed0a46
daneng4 <daneng4@naver.com>
2024-06-10 22:43:07 +0900

    Initial commit

---
+++ src/main/java/com/example/mytistory/MytistoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mytistory/MytistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
