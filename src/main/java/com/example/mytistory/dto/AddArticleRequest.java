c3505c61e4eb278124a806f65ba984069f431d53
daneng4 <daneng4@naver.com>
2024-06-10 23:30:58 +0900

    feat: AddArticleRequest dto 생성
    블로그 글 추가할 때 저장할 Entity로 변환하는 용도

---
+++ src/main/java/com/example/mytistory/dto/AddArticleRequest.java
@@ -0,0 +1,29 @@
+package com.example.mytistory.dto;
+
+import com.example.mytistory.domain.Article;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+
+    private String title;
+    private String content;
+    private String category;
+    private LocalDateTime postTime;
+
+    // DTO -> Entity 메서드, 블로그 글 추가 용도
+    public Article toEntity(){
+        return Article.builder()
+            .title(title)
+            .content(content)
+            .category(category)
+            .postTime(postTime)
+            .build();
+    }
+    
+}
