8c2db9bfcea20f7e15d062e6203d936b232fef61
daneng4 <daneng4@naver.com>
2024-06-17 11:45:49 +0900

    feat; 블로그 글 조회 기능 추가

--- src/main/java/com/example/mytistory/service/ArticleService.java
+++ src/main/java/com/example/mytistory/service/ArticleService.java
@@ -3,6 +3,7 @@ package com.example.mytistory.service;
 import com.example.mytistory.domain.Article;
 import com.example.mytistory.dto.AddArticleRequest;
 import com.example.mytistory.repository.ArticleRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,11 @@ public class ArticleService {
     // 블로그 글 추가
     public Article save(AddArticleRequest request){
         return articleRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 조회
+    public List<Article> findAll(){
+        return articleRepository.findAll();
     }
 
 }
