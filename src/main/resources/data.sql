6ede1046c95cca9b0adfa4049e584d8762fe3831
daneng4 <daneng4@naver.com>
2024-06-17 20:53:58 +0900

    feat: data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(title, content, category) VALUES ('제목1','내용1','카테고리1')
+INSERT INTO article(title, content, category) VALUES ('제목2','내용2','카테고리2')
+INSERT INTO article(title, content, category) VALUES ('제목3','내용3','카테고리3')
