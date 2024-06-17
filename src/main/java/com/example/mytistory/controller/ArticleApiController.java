703815f946a5aae7fcbd2cff22c8ec32c225daa4
daneng4 <daneng4@naver.com>
2024-06-17 11:56:26 +0900

    feat: 블로그 글 목록 조회 API 생성

--- src/main/java/com/example/mytistory/controller/ArticleApiController.java
+++ src/main/java/com/example/mytistory/controller/ArticleApiController.java
@@ -2,10 +2,15 @@ package com.example.mytistory.controller;
 
 import com.example.mytistory.domain.Article;
 import com.example.mytistory.dto.AddArticleRequest;
+import com.example.mytistory.dto.ArticleResponse;
 import com.example.mytistory.service.ArticleService;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +30,14 @@ public class ArticleApiController {
             .body(savedArticle);
     }
 
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = articleService.findAll()
+            .stream()
+            .map(ArticleResponse::new)
+            .toList();
+
+        return ResponseEntity.ok()
+            .body(articles);
+    }
 }
