8b6de5020d8f74052b080379d120c48ed694eb20
daneng4 <daneng4@naver.com>
2024-06-18 14:44:36 +0900

    feat: 블로그 글 수정 API 생성

--- src/main/java/com/example/mytistory/controller/ArticleApiController.java
+++ src/main/java/com/example/mytistory/controller/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.example.mytistory.controller;
 import com.example.mytistory.domain.Article;
 import com.example.mytistory.dto.AddArticleRequest;
 import com.example.mytistory.dto.ArticleResponse;
+import com.example.mytistory.dto.UpdateArticleRequest;
 import com.example.mytistory.service.ArticleService;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,9 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +42,15 @@ public class ArticleApiController {
 
         return ResponseEntity.ok()
             .body(articles);
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable Long id, @RequestBody
+        UpdateArticleRequest request){
+
+        Article updatedArticle = articleService.update(id, request);
+
+        return ResponseEntity.ok()
+            .body(updatedArticle);
     }
 }
