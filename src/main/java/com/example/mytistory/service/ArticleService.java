661b227c4b2c0317377dd6b66771e9ed27aff97d
daneng4 <daneng4@naver.com>
2024-06-18 14:44:56 +0900

    feat: 블로그 글 수정, 글 목록 조회 메서드 생성

--- src/main/java/com/example/mytistory/service/ArticleService.java
+++ src/main/java/com/example/mytistory/service/ArticleService.java
@@ -2,10 +2,12 @@ package com.example.mytistory.service;
 
 import com.example.mytistory.domain.Article;
 import com.example.mytistory.dto.AddArticleRequest;
+import com.example.mytistory.dto.UpdateArticleRequest;
 import com.example.mytistory.repository.ArticleRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +21,28 @@ public class ArticleService {
     }
 
     // 블로그 글 조회
+    public Article findById(Long id){
+        return articleRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    // 블로그 글 목록 조회
     public List<Article> findAll(){
         return articleRepository.findAll();
     }
+
+    // 블로그 글 수정
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = articleRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent(), request.getCategory());
+
+        return article;
+    }
+
+    // 블로그 글 삭제
+
 
 }
