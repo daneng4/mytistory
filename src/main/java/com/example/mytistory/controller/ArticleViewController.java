709804da32bd0beea502ae7863208858b4fd4b61
daneng4 <daneng4@naver.com>
2024-06-20 23:42:36 +0900

    feat: 작성된 글 조회 API 생성

--- src/main/java/com/example/mytistory/controller/ArticleViewController.java
+++ src/main/java/com/example/mytistory/controller/ArticleViewController.java
@@ -1,12 +1,15 @@
 package com.example.mytistory.controller;
 
+import com.example.mytistory.domain.Article;
 import com.example.mytistory.dto.ArticleListViewResponse;
+import com.example.mytistory.dto.ArticleViewResponse;
 import com.example.mytistory.service.ArticleService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +26,14 @@ public class ArticleViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = articleService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 
 }
