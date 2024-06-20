3f3006833681c04b1631b9dc20d63fc3e0da10df
daneng4 <daneng4@naver.com>
2024-06-20 23:32:49 +0900

    fix: test 데이터 셋 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(title, content, category) VALUES ('제목1','내용1','카테고리1')
-INSERT INTO article(title, content, category) VALUES ('제목2','내용2','카테고리2')
-INSERT INTO article(title, content, category) VALUES ('제목3','내용3','카테고리3')
+INSERT INTO article(title, content, category, created_at, updated_at) VALUES ('제목1','내용1','음식',NOW(),NOW())
+INSERT INTO article(title, content, category, created_at, updated_at) VALUES ('제목2','내용2','여행',NOW(),NOW())
+INSERT INTO article(title, content, category, created_at, updated_at) VALUES ('제목3','내용3','공부',NOW(),NOW())
